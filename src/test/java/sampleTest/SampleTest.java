939c6696b844f6092cbcf6862e345254da560d9e
sujana <sujana@LAPTOP-9PSLU48T>
2022-09-14 06:29:15 +0530

    Branch TC_01- Engg1 by commit 1

---
+++ src/test/java/sampleTest/SampleTest.java
@@ -0,0 +1,13 @@
+package sampleTest;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void sampleTest()
+	{
+		System.out.println("Sample Testby engg1");
+	}
+
+}
