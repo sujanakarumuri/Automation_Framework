fecd9d5352eaf2e3682e1c261c137242ba9f9b0c
sujana <sujana@LAPTOP-9PSLU48T>
2022-09-15 07:04:48 +0530

    Commit by engg2 in demoTest

--- src/test/java/sampleTest/DemoTest.java
+++ src/test/java/sampleTest/DemoTest.java
@@ -8,7 +8,7 @@ public class DemoTest {
 	public void demoTest()
 	{
 		System.out.println("This is demo");
-		System.out.println("Done by engg1");
+		System.out.println("Commit test by engg2");
 	}
 
 }
